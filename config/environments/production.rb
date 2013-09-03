468db0505515e2bfab8642c861e09e48b6d0e4c9
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-09-03 22:18:51 +0900

    Fix mistake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Shurijp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.assets.initialize_on_precompile = false
+  config.assets.compile = true
 
   config.action_mailer.default_url_options = { :host => 'http://guarded-basin-4478.herokuapp.com/' }
 end
