bc1c1e933d8ba2379dd8c07b7be7bb2158abfb5b
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-09-25 22:36:57 +0900

    Fix Issue#4

--- app/models/conn.rb
+++ app/models/conn.rb
@@ -1,5 +1,5 @@
 class Conn < ActiveRecord::Base
   belongs_to :user
   has_many :virtual_machines
-  attr_accessible :access_key, :end_point, :secret_access_key, :provider
+  attr_accessible :access_key, :end_point, :secret_access_key, :provider, :name
 end
