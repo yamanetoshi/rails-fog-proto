857fbadbd92aa91e018de880b158d3ccd976f46f
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-10-19 20:57:16 +0900

    debug

--- app/controllers/vm_operations_controller.rb
+++ app/controllers/vm_operations_controller.rb
@@ -4,7 +4,8 @@ require 'json'
 class VmOperationsController < ApplicationController
   before_filter :authenticate_user!
 #  skip_before_filter :verify_authenticity_token, :only => [:create]
-  protect_from_forgery :except => [:create]
+#  protect_from_forgery :except => [:create]
+  protect_from_forgery :only => ["index", "new", "start", "stop", "reboot"]
 #  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 
   def index
