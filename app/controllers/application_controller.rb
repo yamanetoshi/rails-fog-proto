41d002c8cf5991551d9a7283769b71e882e312f7
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-10-19 15:34:22 +0900

    modify calling protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 #  skip_before_filter :verify_authenticity_token
-  protect_from_forgery :secret => 'my-little-pony'
+  protect_from_forgery # :secret => 'my-little-pony'
 
   def admin_login
     unless current_user.try(:admin?)
--- app/controllers/vm_operations_controller.rb
+++ app/controllers/vm_operations_controller.rb
@@ -4,7 +4,7 @@ require 'json'
 class VmOperationsController < ApplicationController
   before_filter :authenticate_user!
 #  skip_before_filter :verify_authenticity_token, :only => [:create]
-  protect_from_forgery :except => ["create"]
+#  protect_from_forgery :except => ["create"]
 
   def index
     session[:idx] = params[:idx]
