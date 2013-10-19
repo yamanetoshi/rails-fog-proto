1544d78dc8bd67b808a7e74a4fe0ed0d8992249d
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-10-19 19:10:52 +0900

    Modify application_controller

--- app/controllers/vm_operations_controller.rb
+++ app/controllers/vm_operations_controller.rb
@@ -5,7 +5,7 @@ class VmOperationsController < ApplicationController
   before_filter :authenticate_user!
 #  skip_before_filter :verify_authenticity_token, :only => [:create]
 #  protect_from_forgery :except => ["create"]
-  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+#  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 
   def index
     session[:idx] = params[:idx]
