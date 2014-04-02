c08b052fe983c798aa34865379cefbebf2e80e8b
YAMANE Toshiaki <yamanetoshi@gmail.com>
2014-04-02 15:48:37 +0900

    Fix Issue #55

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,3 +1,5 @@
+#Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
 RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :controller
   config.include ControllerMacros, :type => :controller
